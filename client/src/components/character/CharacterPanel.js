70e20689626b26da4db19538dbceda1661495257
Alex Williams <Zizzs17@gmail.com>
2020-01-01 19:17:34 -0800

    CSS Updates
    Updated the Character Panel and Nav Bar CSS

--- client/src/components/character/CharacterPanel.js
+++ client/src/components/character/CharacterPanel.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import "./CharacterPanel.css";
+import faceImage from "../../images/blankFace.png";
 
 class CharacterPanel extends Component {
   render() {
@@ -16,23 +17,38 @@ class CharacterPanel extends Component {
 
     return (
       <div id="characterPanel" className={visibility}>
-        <p>Name: {user.name}</p>
-        <p>Health: {user.character.health}</p>
-        <p>Max Health: {user.character.maxHealth}</p>
-        <p>Mana: {user.character.mana}</p>
-        <p>Max Mana: {user.character.maxMana}</p>
-        <p>Strength: {user.character.strength}</p>
-        <p>Dexterity: {user.character.dexterity}</p>
-        <p>Intellect: {user.character.intellect}</p>
-        <p>Luminosity: {user.character.luminosity}</p>
-        <p>Spark: {user.character.spark}</p>
-        <p>Enlightenment: {user.character.enlightenment}</p>
-        <p>Current Energy: {user.character.currentEnergy}</p>
-        <p>Max Energy: {user.character.maxEnergy}</p>
-        <p>Bound Fragments: {user.character.boundFragments}</p>
-        <p>Unbound Fragments: {user.character.unboundFragments}</p>
-        <p>Item Count: {user.character.items.length}</p>
-        <p>Equipped Items: {user.character.equippedItems.length}</p>
+        <div id="characterPanelHeader">
+          <img id="imagePic" src={faceImage} />
+          <p id="characterName">{user.name}</p>
+        </div>
+        <div id="healthMana">
+          <p>Health: {user.character.health}</p>
+          <p>Max Health: {user.character.maxHealth}</p>
+          <p>Mana: {user.character.mana}</p>
+          <p>Max Mana: {user.character.maxMana}</p>
+        </div>
+        <div id="mainStats">
+          <p>Strength: {user.character.strength}</p>
+          <p>Dexterity: {user.character.dexterity}</p>
+          <p>Intellect: {user.character.intellect}</p>
+        </div>
+        <div id="extraStats">
+          <p>Luminosity: {user.character.luminosity}</p>
+          <p>Spark: {user.character.spark}</p>
+          <p>Enlightenment: {user.character.enlightenment}</p>
+        </div>
+        <div id="energyStats">
+          <p>Current Energy: {user.character.currentEnergy}</p>
+          <p>Max Energy: {user.character.maxEnergy}</p>
+        </div>
+        <div id="fragmentStats">
+          <p>Bound Fragments: {user.character.boundFragments}</p>
+          <p>Unbound Fragments: {user.character.unboundFragments}</p>
+        </div>
+        <div id="itemStats">
+          <p>Item Count: {user.character.items.length}</p>
+          <p>Equipped Items: {user.character.equippedItems.length}</p>
+        </div>
       </div>
     );
   }
